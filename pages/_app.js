4ba3649a57d0c0aaf3cc3d5d315a24c308c5267b
ayushraj706 <236733393+ayushraj706@users.noreply.github.com>
2026-06-30 20:21:06 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 // pages/_app.js
+import Head from 'next/head';
 import '../src/styles/globals.css';
 import { AuthProvider } from '../src/context/AuthContext';
 import { ThemeProvider } from '../src/context/ThemeContext';
@@ -8,6 +9,13 @@ export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider>
       <AuthProvider>
+        {/* Head component yahan add kiya hai */}
+        <Head>
+          <title>GamePlatform</title>
+          <link rel="icon" href="/logo.png" />
+          <link rel="apple-touch-icon" href="/logo.png" />
+        </Head>
+        
         <Toaster position="top-center" />
         <Component {...pageProps} />
       </AuthProvider>
