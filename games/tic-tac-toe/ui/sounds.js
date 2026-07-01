89e887aa0fba25e3141862fe5d185346ee290904
ayushraj706 <236733393+ayushraj706@users.noreply.github.com>
2026-07-01 17:13:58 +0530

    Create sounds.js

---
+++ games/tic-tac-toe/ui/sounds.js
@@ -0,0 +1,16 @@
+// games/tic-tac-toe/ui/sounds.js
+
+export const SOUNDS = {
+  // Pop sound for valid move
+  move: 'https://assets.mixkit.co/active_storage/sfx/2568/2568-preview.mp3', 
+  
+  // Winning fanfare
+  checkmate: 'https://assets.mixkit.co/active_storage/sfx/2018/2018-preview.mp3', 
+  
+  // Neutral tone for draw
+  draw: 'https://assets.mixkit.co/active_storage/sfx/2573/2573-preview.mp3', 
+  
+  // Error beep for invalid click
+  invalid: 'https://assets.mixkit.co/active_storage/sfx/2572/2572-preview.mp3' 
+};
+
